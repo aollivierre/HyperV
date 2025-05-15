965443ac01b29bc1d3b5d7c973a549bbc5db6493
aollivierre <abdullahollivierre@gmail.com>
2025-05-15 15:04:12 -0500

    Configure RD Gateway

--- OPNSense/remote-ssh-launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# This script is used to launch remote SSH sessions on OPNsense
-# It works around the lack of bash on FreeBSD-based systems
-
-# Set environment variables
-export SHELL=/bin/sh
-
-# Execute the command that's passed
-exec /bin/sh "$@"
--- OPNSense/ssh-lan.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0ssh-opnsense.ps1" -Target lan
